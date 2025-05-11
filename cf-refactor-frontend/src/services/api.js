83f531ebc62ab4d2e1bde537e2dbca367a8a9606
Adarsh Kumar Singh <0902cs221005@rjit.ac.in>
2025-05-12 03:04:15 +0530

    api/

--- cf-refactor-frontend/src/services/api.js
+++ cf-refactor-frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Define the base URL for your backend API
 // const API_BASE_URL = 'http://localhost:8000/api/';
-const API_BASE_URL = 'https://cf-refactor-backend.onrender.com';
+const API_BASE_URL = 'https://cf-refactor-backend.onrender.com/api/';
 
 export const fetchTopSubmissions = async (cf_id) => {
     try {
